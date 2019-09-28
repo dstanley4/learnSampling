a2f98a88ce87fe7377ed653219f9f741b85bf1b3
David Stanley <dstanley@uoguelph.ca>
2019-09-28 16:15:37 -0400

    edits to make it possible for class to create population correlation data set

--- R/utility.R
+++ R/utility.R
@@ -1,9 +1,8 @@
-#' Create simulated data
-#' @param n Population d-value
-#' @param mu Population d-value
-#' @param sd Population d-value
-#' @param cormatrix Population d-value
-#' @param empirical Population d-value
+#' Create simulated correlatio data
+#' @param n Sample size
+#' @param mu Sample mean values
+#' @param sd Sample sd values
+#' @param cormatrix Sample correlation matrix
 #' @return Data frame with desired properties
 #' @export
 mvrnorm_cor <- function(n = 1, mu, sd, cormatrix, empirical = FALSE) {
@@ -23,4 +22,30 @@ mvrnorm_cor <- function(n = 1, mu, sd, cormatrix, empirical = FALSE) {
                               empirical = empirical)
 
  return(dataout)
+}
+
+
+#' Create simulated data
+#' @param rho Population correlation
+#' @param n Population size
+#' @param mu Population mean values
+#' @param sd Population sd values
+#' @return Data frame with desired properties
+#' @export
+make_pop_cor <- function(n = 1000000, rho = .30, mu = c(0,0), sd = c(1,1)) {
+        cormatrix <- diag(2)
+        cormatrix[1,2] <- rho
+        cormatrix[2,1] <- rho
+
+        my_data <- mvrnorm_cor(n = n,
+                               mu = mu,
+                               sd = sd,
+                               cormatrix = cormatrix,
+                               empirical = TRUE)
+
+
+        my_data <- as.data.frame(my_data)
+        names(my_data) <- c("x", "y")
+        return(my_data)
+
 }
